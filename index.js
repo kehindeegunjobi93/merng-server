cc567d420e7073f5f72a8b227f46e11bc9936f53
Kehinde <kehindeegunjobi93@yahoo.com>
2021-10-19 17:01:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config')
+const typeDefs = require('./graphql/typedefs')
+const resolvers = require('./graphql/resolvers')
+
+const PORT = process.env.port || 5000
+
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req }) 
+});
+
+mongoose.connect(MONGODB, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('MongoDB Connected')
+    return server.listen({port: PORT })
+}).then(res => {
+        console.log(`Server running at ${res.url}`)
+    }).catch(err => {
+        console.log(err)
+    })
